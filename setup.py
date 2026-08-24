1c874fede90abb18651ed44d54db9c9d42aeda09
Felix Laplante <felixlaplante0@proton.me>
2026-08-24 23:22:39 +0200

    fix(windows): pass clang path to cibuildwheel

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ LINK_ARGS = [] if sys.platform == "win32" else ["-pthread"]
 
 class ClangBuildExt(build_ext):
     def build_extensions(self):
-        clang_cl = os.environ.get("FASTKMEANSPP_CLANG_CL")
+        clang_cl = os.environ.get("CLANG_CL")
         if clang_cl:
             self.compiler.initialize()
             self.compiler.cc = clang_cl
