fe9b8284122545f810cf02bf064658224b613dd1
Felix Laplante <felixlaplante0@proton.me>
2026-08-24 14:25:38 +0200

    build: replace Cython with pybind11

---
+++ fastkmeanspp/_highway_bindings.cpp
@@ -0,0 +1,137 @@
+#include "_highway_kernel.h"
+
+#include <cstddef>
+#include <cstdint>
+#include <string>
+
+#include <pybind11/numpy.h>
+#include <pybind11/pybind11.h>
+
+namespace py = pybind11;
+
+namespace {
+
+using FloatArray = py::array_t<float, py::array::c_style>;
+using LabelArray = py::array_t<std::int64_t, py::array::c_style>;
+using StridedFloatArray = py::array_t<float, 0>;
+
+void check_ndim(const py::buffer_info &array, int ndim) {
+  if (array.ndim != ndim) {
+    throw py::value_error("expected an array with " + std::to_string(ndim) +
+                          " dimensions");
+  }
+}
+
+class KMeansWorker {
+ public:
+  explicit KMeansWorker(std::size_t n_jobs) : pool_(fastkmeanspp::create_pool(n_jobs)) {}
+  ~KMeansWorker() { fastkmeanspp::destroy_pool(pool_); }
+
+  py::array_t<float> operator()(const FloatArray &x, const FloatArray &y) const {
+    const auto x_info = x.request();
+    const auto y_info = y.request();
+    check_ndim(x_info, 2);
+    check_ndim(y_info, 2);
+    py::array_t<float> out({x_info.shape[0], y_info.shape[0]});
+    auto out_info = out.request();
+    {
+      py::gil_scoped_release release;
+      fastkmeanspp::dispatch_cdist(
+          static_cast<const float *>(x_info.ptr),
+          static_cast<const float *>(y_info.ptr),
+          static_cast<float *>(out_info.ptr),
+          static_cast<std::size_t>(x_info.shape[0]),
+          static_cast<std::size_t>(y_info.shape[0]),
+          static_cast<std::size_t>(x_info.shape[1]), nullptr, 0, nullptr,
+          pool_);
+    }
+    return out;
+  }
+
+  py::tuple minimum(const FloatArray &x, const FloatArray &y,
+                    const StridedFloatArray &minimums) const {
+    const auto x_info = x.request();
+    const auto y_info = y.request();
+    const auto minimums_info = minimums.request();
+    check_ndim(x_info, 2);
+    check_ndim(y_info, 2);
+    check_ndim(minimums_info, 1);
+    py::array_t<float> out({x_info.shape[0], y_info.shape[0]});
+    py::array_t<float> inertias(y_info.shape[0]);
+    auto out_info = out.request();
+    auto inertias_info = inertias.request();
+    {
+      py::gil_scoped_release release;
+      fastkmeanspp::dispatch_cdist(
+          static_cast<const float *>(x_info.ptr),
+          static_cast<const float *>(y_info.ptr),
+          static_cast<float *>(out_info.ptr),
+          static_cast<std::size_t>(x_info.shape[0]),
+          static_cast<std::size_t>(y_info.shape[0]),
+          static_cast<std::size_t>(x_info.shape[1]),
+          static_cast<const float *>(minimums_info.ptr),
+          static_cast<std::size_t>(minimums_info.strides[0] / sizeof(float)),
+          static_cast<float *>(inertias_info.ptr), pool_);
+    }
+    return py::make_tuple(out, inertias);
+  }
+
+  void lloyd(const FloatArray &x, const FloatArray &centers,
+             const LabelArray &labels) const {
+    const auto x_info = x.request();
+    const auto centers_info = centers.request();
+    const auto labels_info = labels.request();
+    check_ndim(x_info, 2);
+    check_ndim(centers_info, 2);
+    check_ndim(labels_info, 1);
+    {
+      py::gil_scoped_release release;
+      fastkmeanspp::dispatch_lloyd(
+          static_cast<const float *>(x_info.ptr),
+          static_cast<float *>(centers_info.ptr),
+          static_cast<std::int64_t *>(labels_info.ptr),
+          static_cast<std::size_t>(x_info.shape[0]),
+          static_cast<std::size_t>(centers_info.shape[0]),
+          static_cast<std::size_t>(x_info.shape[1]), pool_);
+    }
+  }
+
+  void assign(const FloatArray &x, const FloatArray &centers,
+              const LabelArray &labels) const {
+    const auto x_info = x.request();
+    const auto centers_info = centers.request();
+    const auto labels_info = labels.request();
+    check_ndim(x_info, 2);
+    check_ndim(centers_info, 2);
+    check_ndim(labels_info, 1);
+    {
+      py::gil_scoped_release release;
+      fastkmeanspp::dispatch_assign(
+          static_cast<const float *>(x_info.ptr),
+          static_cast<const float *>(centers_info.ptr),
+          static_cast<std::int64_t *>(labels_info.ptr),
+          static_cast<std::size_t>(x_info.shape[0]),
+          static_cast<std::size_t>(centers_info.shape[0]),
+          static_cast<std::size_t>(x_info.shape[1]), pool_);
+    }
+  }
+
+ private:
+  void *pool_;
+};
+
+}  // namespace
+
+PYBIND11_MODULE(_highway, module) {
+  py::class_<KMeansWorker>(module, "KMeansWorker")
+      .def(py::init<std::size_t>(), py::arg("n_jobs"))
+      .def("__call__", &KMeansWorker::operator())
+      .def("minimum", &KMeansWorker::minimum)
+      .def("lloyd", &KMeansWorker::lloyd)
+      .def("assign", &KMeansWorker::assign);
+
+  module.def("cdist", [](const FloatArray &x, const FloatArray &y,
+                          std::size_t n_jobs) {
+    return KMeansWorker(n_jobs)(x, y);
+  }, py::arg("X"), py::arg("y"), py::arg("n_jobs") = 0);
+}
--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ import sys
 from pathlib import Path
 
 import numpy as np
-from Cython.Build import cythonize
-from setuptools import Extension, setup
+from pybind11.setup_helpers import Pybind11Extension, build_ext
+from setuptools import setup
 
 ROOT = Path(__file__).resolve().parent
 HIGHWAY = ROOT / "highway"
@@ -18,30 +18,28 @@ COMPILE_ARGS = ["/O2", "/std:c++17"] if sys.platform == "win32" else [
 LINK_ARGS = [] if sys.platform == "win32" else ["-pthread"]
 
 setup(
-    ext_modules=cythonize(
-        [
-            Extension(
-                "fastkmeanspp._highway",
-                [
-                    "fastkmeanspp/_highway.pyx",
-                    "fastkmeanspp/_highway_kernel.cc",
-                    "highway/hwy/abort.cc",
-                    "highway/hwy/aligned_allocator.cc",
-                    "highway/hwy/contrib/sort/vqsort.cc",
-                    "highway/hwy/contrib/sort/vqsort_have.cc",
-                    "highway/hwy/contrib/sort/vqsort_f64a.cc",
-                    "highway/hwy/contrib/thread_pool/thread_pool.cc",
-                    "highway/hwy/contrib/thread_pool/topology.cc",
-                    "highway/hwy/profiler.cc",
-                    "highway/hwy/targets.cc",
-                    "highway/hwy/timer.cc",
-                ],
-                include_dirs=[np.get_include(), str(HIGHWAY)],
-                extra_compile_args=COMPILE_ARGS,
-                extra_link_args=LINK_ARGS,
-                language="c++",
-            )
-        ],
-        language_level=3,
-    )
+    ext_modules=[
+        Pybind11Extension(
+            "fastkmeanspp._highway",
+            [
+                "fastkmeanspp/_highway_bindings.cpp",
+                "fastkmeanspp/_highway_kernel.cc",
+                "highway/hwy/abort.cc",
+                "highway/hwy/aligned_allocator.cc",
+                "highway/hwy/contrib/sort/vqsort.cc",
+                "highway/hwy/contrib/sort/vqsort_have.cc",
+                "highway/hwy/contrib/sort/vqsort_f64a.cc",
+                "highway/hwy/contrib/thread_pool/thread_pool.cc",
+                "highway/hwy/contrib/thread_pool/topology.cc",
+                "highway/hwy/profiler.cc",
+                "highway/hwy/targets.cc",
+                "highway/hwy/timer.cc",
+            ],
+            include_dirs=[np.get_include(), str(HIGHWAY), str(ROOT / "fastkmeanspp")],
+            extra_compile_args=COMPILE_ARGS,
+            extra_link_args=LINK_ARGS,
+            cxx_std=17,
+        )
+    ],
+    cmdclass={"build_ext": build_ext},
 )
